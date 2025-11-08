039aa220ae48efef80b679f520f8aca8a4d1e2f3
maehiyu <m.hiyu3040@gmail.com>
2025-11-08 17:42:38 +0900

    feature: .gradlewで生成可能に

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+// 依存ライブラリを探す場所を定義します
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral() // <- これでkotlin-scripting-compilerが見つかるようになります
+    }
+}
+
+rootProject.name = "tollo"
 include(":client:shared")
